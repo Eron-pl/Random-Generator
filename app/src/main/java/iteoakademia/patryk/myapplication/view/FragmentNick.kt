a232052adc2591243b57b189c668887b1ee1f483
psablik <patryk.sablik@iteo.com>
2022-07-19 14:42:54 +0200

    Added stuff

--- app/src/main/java/iteoakademia/patryk/myapplication/view/FragmentNick.kt
+++ app/src/main/java/iteoakademia/patryk/myapplication/view/FragmentNick.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import iteoakademia.patryk.myapplication.R
@@ -26,16 +27,26 @@ class FragmentNick : Fragment() {
         return inflater.inflate(R.layout.fragment_fragment_nick, container, false)
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FragmentNickViewModel::class.java)
 
+        val animation = AnimationUtils.loadAnimation(this.context, R.anim.fade_in_and_translate)
+
         val btnGenerateNick = getView()?.findViewById<Button>(R.id.btnGenerateNick)
         val tvShowNick = getView()?.findViewById<TextView>(R.id.tvShowNick)
 
-        btnGenerateNick?.setOnClickListener{
+        tvShowNick?.startAnimation(animation)
+        btnGenerateNick?.startAnimation(animation)
+
+        btnGenerateNick?.setOnClickListener {
+            btnGenerateNick?.clearAnimation()
+            btnGenerateNick.translationY = 50.0f
+
             viewModel.generateNick()
             tvShowNick?.text = viewModel.nick
+            tvShowNick?.startAnimation(animation)
         }
     }
 
--- app/src/main/java/iteoakademia/patryk/myapplication/view/FragmentPassword.kt
+++ app/src/main/java/iteoakademia/patryk/myapplication/view/FragmentPassword.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.slider.Slider
 import iteoakademia.patryk.myapplication.R
@@ -32,15 +34,39 @@ class FragmentPassword : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FragmentPasswordViewModel::class.java)
 
+        val animation = AnimationUtils.loadAnimation(this.context, R.anim.fade_in_and_translate)
+
         val btnGeneratePassword = getView()?.findViewById<Button>(R.id.btnGeneratePassword)
         val tvShowPassword = getView()?.findViewById<TextView>(R.id.tvShowPassword)
         val sliderPasswordLength = getView()?.findViewById<Slider>(R.id.sliderPasswordLength)
         val checkBoxSpecialCharacters = getView()?.findViewById<CheckBox>(R.id.checkboxSpecialCharacters)
         val checkBoxNumbers = getView()?.findViewById<CheckBox>(R.id.checkboxNumbers)
+        val llSpecialCharacters = getView()?.findViewById<LinearLayout>(R.id.llSpecialCharacters)
+        val llNumbers = getView()?.findViewById<LinearLayout>(R.id.llNumbers)
+        val llSliderPasswordLength = getView()?.findViewById<LinearLayout>(R.id.llSliderPasswordLength)
+
+        btnGeneratePassword?.startAnimation(animation)
+        tvShowPassword?.startAnimation(animation)
+        llSpecialCharacters?.startAnimation(animation)
+        llNumbers?.startAnimation(animation)
+        llSliderPasswordLength?.startAnimation(animation)
+
 
         btnGeneratePassword?.setOnClickListener {
+            btnGeneratePassword?.clearAnimation()
+            llSpecialCharacters?.clearAnimation()
+            llNumbers?.clearAnimation()
+            llSliderPasswordLength?.clearAnimation()
+            btnGeneratePassword?.translationY = 50.0f
+            llSpecialCharacters?.translationY = 50.0f
+            llNumbers?.translationY = 50.0f
+            llSliderPasswordLength?.translationY = 50.0f
+
+
             viewModel.generatePassword(sliderPasswordLength?.value?.toInt(), checkBoxSpecialCharacters, checkBoxNumbers)
             tvShowPassword?.text = viewModel.password
+            val animation = AnimationUtils.loadAnimation(this.context, R.anim.fade_in_and_translate)
+            tvShowPassword?.startAnimation(animation)
         }
     }
 
